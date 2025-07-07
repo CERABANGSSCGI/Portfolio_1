f26d1f6c6532b683ba958207812bad115deb5c04
cerabang <cerabang@sscgi.com>
2025-07-07 12:46:28 +0800

    Initial Commit and Push

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+  const track = document.getElementById("iconTrack");
+  const icons = track.innerHTML; // Get the original icons
+
+  // Duplicate once
+  track.innerHTML += icons;
+</script>
