73f3d9f6cb927b5da76b4be105d96ad7621526ef
cerabang <cerabang@sscgi.com>
2025-07-08 08:26:39 +0800

    Deleted unwanted files

--- script.js
+++
@@ -1,7 +0,0 @@
-<script>
-  const track = document.getElementById("iconTrack");
-  const icons = track.innerHTML; // Get the original icons
-
-  // Duplicate once
-  track.innerHTML += icons;
-</script>
